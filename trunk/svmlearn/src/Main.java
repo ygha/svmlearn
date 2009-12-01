4c5064ff763f8d08fa2526161b57024389211c2d
zalan.bodo <zalan.bodo@b5e03204-ddcf-11de-af3d-4d7ff200f69e>
2009-12-01 12:31:02 +0000

    First version; works on binary problems

--- trunk/svmlearn/src/Main.java
+++ trunk/svmlearn/src/Main.java
@@ -4,6 +4,27 @@ public class Main {
 	public static void main(String [] args) {  
 		SVM s = new SVM();
 		
+		Problem train = new Problem();
+		Problem test = new Problem();
+//		train.loadProblem("G:\\!data\\lshtc\\proba2\\train.txt");
+//		test.loadProblem("G:\\!data\\lshtc\\proba2\\test.txt");
+//		train.loadBinaryProblem("G:\\!data\\lshtc\\reuters\\train.txt");
+//		test.loadBinaryProblem("G:\\!data\\lshtc\\reuters\\test.txt");
+		train.loadBinaryProblem("train.txt");
+		test.loadBinaryProblem("test.txt");
+
+		System.out.println("Loaded!");
+		System.out.println("Training...");
+		s.svmTrain(train);
+		System.out.println("Testing...");
+		int [] pred = s.svmTest(test);
+		for (int i=0; i<pred.length; i++)
+			System.out.println(pred[i]);
+		
+		
+		EvalMeasures e = new EvalMeasures(test, pred, 2);
+		System.out.println("Accuracy=" + e.Accuracy());
+		
 		System.out.println("Done!");
 	}
 }
